a16fe574926cd84208190b53766ce96e5b7c451f
Guilherme <95645198+Guiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2022-03-09 17:23:22 +0000

    Create app.js

---
+++ Ficha1/app.js
@@ -0,0 +1,111 @@
+// console.log("ola mundo");
+/* function nota (nota_pratica, nota_teorica){
+    var media = ((nota_pratica + nota_teorica) / 2);
+    
+    if (media >= 10){
+        console.log("Aprovado")
+    }
+    else{
+        console.log("Reprovado")
+    }
+
+} */
+
+//nota(10, 7);
+
+/* function mes_do_ano (numero_do_mes){
+
+    if(numero_do_mes == 1){
+        console.log("Janeiro")
+    }
+    else if (numero_do_mes == 2){
+        console.log("Fevereiro")
+    }
+    else if (numero_do_mes == 3){
+        console.log("Março")
+    }
+    else if (numero_do_mes == 4){
+        console.log("Abril")
+    }
+    else if (numero_do_mes == 5){
+        console.log("Maio")
+    }
+    else if (numero_do_mes == 6){
+        console.log("Junho")
+    }
+    else if (numero_do_mes == 7){
+        console.log("Julho")
+    }
+    else if (numero_do_mes == 8){
+        console.log("Agosto")
+    }
+    else if (numero_do_mes == 9){
+        console.log("Setembro")
+    }
+    else if (numero_do_mes == 10){
+        console.log("Outubro")
+    }
+    else if (numero_do_mes == 11){
+        console.log("Novembro")
+    }
+    else if (numero_do_mes == 12){
+        console.log("Dezembro")
+    }
+    else {
+        console.log("Numero inválido")
+    }
+    
+}
+mes_do_ano(13); */
+
+/* function operador (numero1,numero2,operador){
+    if (operador == "+"){
+        resultado = numero1+numero2
+        console.log(resultado)
+    }
+    else if (operador == "-"){
+        resultado = numero1-numero2
+        console.log(resultado)
+    }
+    else if (operador == "/"){
+        resultado = numero1/numero2
+        console.log(resultado)
+    }
+    if (operador == "**"){
+        resultado = numero1**numero2
+        console.log(resultado)
+    }
+}
+operador(2,2,"**") */
+
+/* function imprimir(numero){
+    var passo = 1
+    resultado = numero
+    while (resultado < 20){
+        multiplos = numero * passo
+        console.log(multiplos)
+        passo += 1
+        resultado += numero
+    }
+}
+imprimir(5);
+ */
+/* function numbers1to100 (value){
+    var s =0;
+    for (var i= 1; i<=value; i++){
+        s+=i
+    }
+    return s;
+}
+
+var something = sum(100);
+ */
+function fatorial(n){
+    var fatorial = 5;
+var resultado = fatorial;
+for (var i = 1; i < fatorial; i++) {
+    resultado *= i;
+    }
+}
+
+console.log(resultado);
