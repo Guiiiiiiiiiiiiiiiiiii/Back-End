13056c0ec99b4599872c8ea19e001d296b51e127
Guilherme <95645198+Guiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2022-03-16 20:50:36 +0000

    Update exe.js

--- Ficha2/exe.js
+++ Ficha2/exe.js
@@ -28,7 +28,7 @@ IMC(70,1.83); */
 }
 reverse("Hoje e Domingo"); */
 
-function vogais(str){
+/* function vogais(str){
     var count = 0;
     for(var i=0; i<str.length; i++){
         if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="0" || str[i]=="u"){
@@ -38,4 +38,47 @@ function vogais(str){
     return count;
 }
 vogais("Ola mundo");
-console.log(count);
+console.log(count); */
+
+/* function letra(str, letter){
+    var count = 0
+    str = str.toLowerCase();
+    for(var i=0; i<str.length; i++){
+        if (str[i] == letter){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(letra("Guilherme","e")); */
+
+/* function work(hentrada, mentrada, sentrada,hsaida, msaida,ssaida){
+    var entrada = hentrada*3600 + mentrada*60 + sentrada
+    var saida = hsaida*3600 + msaida*60 + ssaida
+
+    var segundostrabalho = entrada - saida
+
+    var horasTra= Math.floor(segundostrabalho / 3600);
+    var horasRes= segundostrabalho % 3600;
+
+    var minTra = Math.floor(horasRes / 60);
+    var segTra = horasRes % 60;
+
+    console.log("Trabalhou" + horasTra + "h" + minTra + "m" + segTra + "s")
+}
+work(8,0,0,9,1,1) */
+
+/* function retangulo(largura, altura){
+    var linha ="";
+    for(var i=0; i<largura; i++){
+        linha+="*";
+    }
+    for(var j=0; j<altura; j++){
+        console.log(linha);
+    }
+}
+retangulo(20,10) */
+
+function triangulo(altura){
+    for(var i=0; i<altura)
+}
