70b0a1a0bdbeeb8d38deab8c944b35db8b336da2
Guilherme <95645198+Guiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2022-03-23 19:59:30 +0000

    Update exe.js

--- Ficha2/exe.js
+++ Ficha2/exe.js
@@ -80,11 +80,134 @@ work(8,0,0,9,1,1) */
 retangulo(20,10) */
 
 /* function triangulo(altura){
-    var linha="";
+    var linha="*"
     for(var i=0; i<altura;i++){
-        linha+="*"
         console.log(linha)
+        linha+="*"
     }
 }
 triangulo(10) */
 
+/* function box(width,heigth){
+
+    for(let i=0; i<heigth;i++){
+        var line =""
+        for(let j=0; j<width;j++){
+            if(i==0 || i==heigth-1){
+                line+="*";
+            }
+            else{
+                if(j==0 || j==width-1){
+                    line+="*"
+                }
+                else{
+                    line+=" "
+                }
+            }
+        }
+        console.log(line)
+    }
+}
+box(10,10) */
+
+
+
+
+var students = [];
+
+for(let i=0; i<100;i++){
+    var student = {
+        number: Math.ceil(Math.random()*1000), 
+        grade: Math.random()*20
+    };
+    student.push(student)
+}
+
+function listAll(students){
+    for(let i=0; i< students.length;i++){
+        var student = students[i];
+        console.log("Number:" + student.number
+         + ", Grade" + student.grade)
+    }
+}
+function bestGrade(students){
+    var biggestGrade = students[0].grade;
+    for(var i=0; i<students.length; i++){
+        if(biggestGrade > students[i].grade){
+            biggestGrade = students[i].grade
+        }
+    }
+    return biggestGrade
+}
+function worstGrade(students){
+    var lowestGrade = students[0].grade;
+    for (let i = 0; i < students.length; i++) {
+        if (lowestGrade > students[i].grade){
+            lowestGrade = students[i].grade;
+        }
+    }
+    return lowestGrade
+}
+function averageGrade(students){
+    var sum = 0;
+    for(var i=0; i<students.length; i++){
+        sum += students[i].grade;
+    }
+    return sum/students.legth()
+}
+function closeAverage(students){
+    var avg= averageGrade(students);
+    var resultado= Math.abs(students[0].grade - avg);
+    for(var i=0;i<students.length; i++){
+        if(students[i].grade - avg < resultado){
+            resultado = students[i].grade - avg
+        }
+    }
+    return resultado;
+}
+
+function negativesGrade(students){
+    var sum= 0;
+    for (var i=0; i<students.length; i++){
+        if (students[i].grade < 9.5){
+            sum ++;
+        }
+    }
+    return sum
+}
+function positiveGrade(students){
+    var sum= 0;
+    for (var i=0; i<students.length; i++){
+        if (students[i].grade >= 9.5){
+            sum ++;
+        }
+    }
+    return sum
+}
+
+function studentsStats(students, option){
+    switch (option) {
+        case 1:
+            listAll(students);
+            break;
+        case 2:
+            bestGrade(students);
+            break;
+        case 3:
+            worstGrade(students);
+            break;
+        case 4:
+            averageGrade(students);
+            break;
+        case 5: 
+            negativesGrade(students);
+            break;
+        case 6:
+            positiveGrade(students);
+        default:
+            console.log("Invalid option");
+            break;
+        
+    }
+}
+studentsStats(students,1);
