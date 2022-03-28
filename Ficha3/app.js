b0cc867e577dff87fdd8c2d7fe582d5a7f92612c
Guilherme <95645198+Guiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2022-03-28 19:39:45 +0100

    Update app.js

--- Ficha3/app.js
+++ Ficha3/app.js
@@ -1,3 +1,16 @@
-function performDownload(started, update, completed){
-    
+function started(){
+    console.log("Started Download")
 }
+function update(progress){
+    console.log(progress + "% of Download" )
+}
+function completed(){
+    console.log("Finished Download")
+}
+function performDownload(started_fn, update_fn, completed_fn){
+    for(var i=0; i<=100;i++){
+        update_fn(i);
+    }
+    completed_fn();
+}
+performDownload(started,update, completed)
